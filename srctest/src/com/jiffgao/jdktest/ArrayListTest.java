f83142f42c96f827ce61a304d1b79ed66f9a447f
gaojinfu <gaojinfu@sinosoft.com.cn>
2019-11-29 13:56:14 +0800

    1

---
+++ srctest/src/com/jiffgao/jdktest/ArrayListTest.java
@@ -0,0 +1,17 @@
+package com.jiffgao.jdktest;
+
+import java.util.ArrayList;
+
+/**
+ * @author Administrator
+ * @ClassName: ArrayListTest
+ * @Description:
+ * @date: 2019/11/29
+ */
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList abstractCollection = new ArrayList();
+        abstractCollection.add("fdaskf");
+        abstractCollection.clear();
+    }
+}
