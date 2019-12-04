22d3384dc6e7c911236073f4edb4c6143756747c
gaojinfu <gaojinfu@sinosoft.com.cn>
2019-12-04 17:59:34 +0800

    HashMap

--- srctest/src/com/jiffgao/jdktest/HashMapTest.java
+++ srctest/src/com/jiffgao/jdktest/HashMapTest.java
@@ -1,7 +1,6 @@
 package com.jiffgao.jdktest;
 
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Administrator
@@ -19,16 +18,29 @@ public class HashMapTest {
         // n |= n >>> 16;
         // System.out.println(n);
         // System.out.println(1 << 30);
-        Map map = new HashMap();
-        map.put("1","a");
-        map.put("2","b");
-        map.put("3","c");
-        map.put("4","d");
-        map.put("5","e");
-        map.put("6","f");
-        map.put("7","g");
-        map.put("8","h");
-        map.put("9","i");
-        new HashMap(map);
+        MyHashMap map = new MyHashMap();
+        map.put("b","ww");
+        map.put("200","nihao");
+        map.put("211","www");
+        map.put("222","baidu");
+        map.put("233","ali");
+        map.put("244","tencent");
+        map.put("266","sou");
+        map.put("277","hu");
+        map.put("288","ai");
+        map.put("299","qi");
+        map.put("189","world");
+        map.put("r","ee");
+        map.put("13","bfr");
+        map.put("24","aa");
+        map.put("57","bb");
+        map.put("123","cc");
+        map.put("134","cttc");
+        map.put("145","cttkc");
+        map.put("156","rew");
+        map.put("178","hello");
+        map.put("255","dd");
+        map.put("497","ff");
+        map.put("1017","hh");
     }
 }
